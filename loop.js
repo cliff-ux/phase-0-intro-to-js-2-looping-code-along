6c8278d8047181b31a68ff3d81005850399be337
cliff <rydleecliff@gmail.com>
2024-07-10 09:25:45 +0300

    scope lab

---
+++ loop.js
@@ -0,0 +1,13 @@
+
+function writeCards(names) {
+    let thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYouMessages.push(Thanks, $ ('names,')['i]}, for the wonderful '['surprise'],' gift!']('expect,'));
+        }
+    return thankYouMessages;
+}
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
